2bf36c5f157895cd83fa1c5591745ffa57a7dea5
IvanShmytov <ivanshmytov@mail.ru>
2022-05-05 14:43:25 +0300

    Добавьте файлы проекта.

---
+++ Calculator.cs
@@ -0,0 +1,44 @@
+﻿//Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. 
+//Определите интерфейс для функции сложения числа и реализуйте его.
+//Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.
+namespace Modul10Task1
+{
+    class Calculator : ICalculator
+    {
+        public void Div()
+        {
+            Console.WriteLine("Операция деления\nВведите первый операнд");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй операнд");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:\n" + (a / b));
+        }
+
+        public void Mult()
+        {
+            Console.WriteLine("Операция умножения\nВведите первый операнд");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй операнд");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:\n" + (a * b));
+        }
+
+        public void Subtr()
+        {
+            Console.WriteLine("Операция вычитания\nВведите первый операнд");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй операнд");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:\n" + (a - b));
+        }
+
+        public void Sum()
+        {
+            Console.WriteLine("Операция сложения\nВведите первый операнд");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второй операнд");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат:\n" + (a + b));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. 
+//Определите интерфейс для функции сложения числа и реализуйте его.
+//Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.
+namespace Modul10Task1
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                ICalculator calc = new Calculator();
+                Console.WriteLine("Выберите операцию\n1. Сложение\n2. Вычитание\n3. Умножение\n4. Деление");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        calc.Sum();
+                        break;
+                    case 2:
+                        calc.Subtr();
+                        break;
+                    case 3:
+                        calc.Mult();
+                        break;
+                    case 4:
+                        calc.Div();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
